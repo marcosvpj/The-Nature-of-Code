8004e189ca1c4b79084ee3b85781675e7158d59f
Marcos V. Pagelkopf Jr <marcos@marcosvpj.com.br>
2016-11-25 13:28:46 -0200

    Refactor Ball

--- vectors/Ball.py
+++ vectors/Ball.py
@@ -6,16 +6,21 @@ from vectors.PVector import PVector
 class Ball:
     def __init__(self):
         self.location = PVector(300, 150)
-
         self.velocity = PVector(2, 2)
 
     def update(self, screen):
-        pygame.draw.circle(screen, (255, 255, 255), self.location.to_tuple(), 10)
+        pygame.draw.circle(screen, (255, 255, 255), tuple(self.location), 10)
 
-        if self.location.x + self.velocity.x > screen.get_width() or self.location.x + self.velocity.x < 0:
+        if self.is_inside_horizontal_boundaries(screen.get_width()):
             self.velocity.x *= -1
 
-        if self.location.y + self.velocity.y > screen.get_height() or self.location.y + self.velocity.y < 0:
+        if self.is_inside_vertical_boundaries(screen.get_height()):
             self.velocity.y *= -1
 
         self.location += self.velocity
+
+    def is_inside_horizontal_boundaries(self, limit):
+        return self.location.x + self.velocity.x > limit or self.location.x + self.velocity.x < 0
+
+    def is_inside_vertical_boundaries(self, limit):
+        return self.location.y + self.velocity.y > limit or self.location.y + self.velocity.y < 0
