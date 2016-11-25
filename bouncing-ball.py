4ee6d5f6ac7eb4fb563daa37c15299d82f625918
Marcos V. Pagelkopf Jr <marcos@marcosvpj.com.br>
2016-11-24 22:49:46 -0200

    Extract Ball class to another file

--- bouncing-ball.py
+++ bouncing-ball.py
@@ -1,39 +1,16 @@
 import engine
-import pygame
+
+from vectors.Ball import Ball
 
 
-class Ball:
+class Loop:
     def __init__(self):
-        self.x = 300
-        self.y = 150
+        self.ball = Ball()
 
-        self.yspeed = 2
-        self.xspeed = 2
-
-    def update(self, screen):
-        pos = (self.x, self.y)
-        pygame.draw.circle(screen, (255, 255, 255), pos, 10)
-
-        if self.x + self.xspeed > screen.get_width():
-            self.xspeed *= -1
-        if self.x + self.xspeed < 0:
-            self.xspeed *= -1
-
-        if self.y + self.yspeed > screen.get_height():
-            self.yspeed *= -1
-        if self.y + self.yspeed < 0:
-            self.yspeed *= -1
-
-        self.x += self.xspeed
-        self.y += self.yspeed
+    def loop(self, screen):
+        screen.fill((0, 0, 0))
+        self.ball.update(screen)
 
 
-ball = Ball()
-
-
-def loop(screen):
-    screen.fill((0, 0, 0))
-    ball.update(screen)
-
-
-engine.main(loop)
+loop = Loop()
+engine.main(loop.loop)
