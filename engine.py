48a5442a4cc32fd2346f6a65defd7ed2e236aaae
Marcos V. Pagelkopf Jr <marcos@marcosvpj.com.br>
2017-01-29 18:14:45 -0200

    Limit FPS

--- engine.py
+++ engine.py
@@ -1,4 +1,5 @@
 import pygame
+import pygame.time
 
 
 def should_keep_running():
@@ -18,6 +19,12 @@ def is_exit_event(event):
     return esc_key_pressed(event) or exit_button_pressed(event)
 
 
+def write(screen, text):
+    myfont = pygame.font.SysFont("monospace", 15)
+    label = myfont.render(text, 1, (255, 255, 0))
+    screen.blit(label, (100, 100))
+
+
 def main(main_loop):
     screen_width = 800
     screen_height = 600
@@ -25,6 +32,10 @@ def main(main_loop):
     screen = pygame.display.set_mode((screen_width, screen_height))
     pygame.init()
 
+    clock = pygame.time.Clock()
+
     while should_keep_running():
+        clock.tick(60)
         pygame.display.flip()
+        # print(clock.get_fps())
         main_loop(screen)
