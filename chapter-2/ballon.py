e1c98d451a32d39cd667c3ac98182b11c70ee941
Marcos V. Pagelkopf Jr <marcos@marcosvpj.com.br>
2017-03-04 22:50:03 -0300

    Change wind force from randrange to Perlin noise

--- chapter-2/ballon.py
+++ chapter-2/ballon.py
@@ -4,23 +4,28 @@ import pygame
 
 import engine
 from vectors.PVector import PVector
+from noise import pnoise2, snoise2, pnoise1
 
 
 class Balloon:
     def __init__(self):
+        self.location = PVector(randrange(50, 750), 580)
         self.velocity = PVector(0, 0)
         self.acceleration = PVector(0, 0)
-        self.location = PVector(randrange(50, 750), 580)
+
         self.velocity_limit = False
         self.color = (randrange(0, 255), randrange(0, 255), randrange(0, 255))
+
+        self.mass = randrange(2, 5)
+
+    def apply_force(self, force):
+        force /= self.mass
+        self.acceleration += force
 
     def update(self, screen):
 
         helium = PVector(0, -1)
-        self.acceleration += helium
-
-        wind = PVector(randrange(-3, 5), randrange(-3, 0))
-        self.acceleration += wind
+        self.apply_force(helium)
 
         self.velocity += self.acceleration
         self.location += self.velocity
@@ -29,7 +34,7 @@ class Balloon:
         if self.velocity_limit is not False:
             self.velocity.limit(self.velocity_limit)
 
-        self.screen_limit()
+        self.top_bounce()
 
         self.draw_on_screen(screen)
 
@@ -40,10 +45,10 @@ class Balloon:
         draw_position = (int(self.location.x), int(self.location.y))
         pygame.draw.circle(screen, self.color, draw_position, 10)
 
-    def screen_limit(self):
+    def top_bounce(self):
         if self.location.y <= 50:
             self.location.y = 50
-            self.acceleration = self.velocity * 3 * -1
+            self.apply_force(self.velocity * self.mass * 3 * -1)
 
 
 class Loop:
@@ -54,11 +59,31 @@ class Loop:
         self.balloon2 = Balloon()
         self.balloon2.limit_velocity(5)
 
+        self.t = randrange(0, 1000)
+        self.base = 0
+        self.points = 256
+        self.span = 5.0
+        self.octaves = 2
+
     def loop(self, screen):
         screen.fill((0, 0, 0))
 
+        noise = self.calculate_noise()
+        noise *= 10
+
+        wind = PVector(noise, randrange(-3, 0))
+
+        self.balloon.apply_force(wind)
+        self.balloon2.apply_force(wind)
+
         self.balloon.update(screen)
         self.balloon2.update(screen)
+
+    def calculate_noise(self):
+        x = float(self.t) * self.span / self.points - 0.5 * self.span
+        noise = pnoise1(x + self.base, self.octaves)
+        self.t += 1
+        return noise
 
 
 main_loop = Loop()
