1aa9e34d3da9fb5c3137fe94133205876197b422
Marcos V. Pagelkopf Jr <marcos@marcosvpj.com.br>
2017-03-04 22:10:36 -0300

    Make ballon bounce on top of screen

--- chapter-2/ballon.py
+++ chapter-2/ballon.py
@@ -10,7 +10,7 @@ class Balloon:
     def __init__(self):
         self.velocity = PVector(0, 0)
         self.acceleration = PVector(0, 0)
-        self.location = PVector(randrange(50, 1000), 580)
+        self.location = PVector(randrange(50, 750), 580)
         self.velocity_limit = False
         self.color = (randrange(0, 255), randrange(0, 255), randrange(0, 255))
 
@@ -29,18 +29,27 @@ class Balloon:
         if self.velocity_limit is not False:
             self.velocity.limit(self.velocity_limit)
 
-        print(self.velocity.y)
+        self.screen_limit()
 
-        draw_position = (int(self.location.x), int(self.location.y))
-        pygame.draw.circle(screen, self.color, draw_position, 10)
+        self.draw_on_screen(screen)
 
     def limit_velocity(self, limit):
         self.velocity_limit = limit
+
+    def draw_on_screen(self, screen):
+        draw_position = (int(self.location.x), int(self.location.y))
+        pygame.draw.circle(screen, self.color, draw_position, 10)
+
+    def screen_limit(self):
+        if self.location.y <= 50:
+            self.location.y = 50
+            self.acceleration = self.velocity * 3 * -1
 
 
 class Loop:
     def __init__(self):
         self.balloon = Balloon()
+        self.balloon.limit_velocity(7)
 
         self.balloon2 = Balloon()
         self.balloon2.limit_velocity(5)
@@ -53,4 +62,4 @@ class Loop:
 
 
 main_loop = Loop()
-engine.main(main_loop.loop, 15, screen_size=(1000,1000))
+engine.main(main_loop.loop, 15, screen_size=(800, 600))
